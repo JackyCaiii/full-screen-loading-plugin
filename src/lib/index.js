2ecaecdb8a3a581ed86638116d4230587947e314
caiyongqi <yongqi.cai@happy-seed.com>
2020-09-01 18:11:02 +0800

    first commit

---
+++ src/lib/index.js
@@ -0,0 +1,19 @@
+import loading from "./FullScreenLoading";
+
+const LoadingPlugin = {
+  install(Vue, options) {
+    const subClass = Vue.extend(loading);
+    const Profile = new subClass({
+      el: document.createElement("div")
+    });
+    document.body.appendChild(Profile.$el);
+    if (options) {
+      if (options.tips) {
+        Profile.tips = options.tips;
+      }
+    }
+    Vue.prototype.$fullScreenLoading = Profile;
+  }
+};
+
+export default LoadingPlugin;
